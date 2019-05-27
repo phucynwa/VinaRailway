a4b063169292551302e8960178d20f81d758d57d
PhucSSK <lhphuc.ynwa@gmail.com>
2019-05-27 08:55:04 +0700

    Init project

---
+++ app/src/main/java/com/example/vinarailway/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.vinarailway.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vinarailway.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
